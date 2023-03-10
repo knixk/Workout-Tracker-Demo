0e5df7d423275af6b65e6b97147edb971b27757d
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-03-10 14:18:30 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const path = require("path")
+const cors = require("cors");
 
 // express app
 const app = express()
-
+const PORT = process.env.PORT || 4000
 // middleware
+app.use(cors());
 app.use(express.json())
 
 // to join both front and backend on deployment
@@ -36,8 +38,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('connected to database')
     // listen to port
-    app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+    app.listen(PORT, () => {
+      console.log('listening for requests on port', PORT)
     })
   })
   .catch((err) => {
