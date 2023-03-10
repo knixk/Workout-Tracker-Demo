94817179769c24203c83dcaf34fa3a21e972d61f
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-03-10 14:13:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const path = require("path")
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+// to join both front and backend on deployment
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       res.status(500).send(err);
+//     }
+//   );
+// });
+
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
